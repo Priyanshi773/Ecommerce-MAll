77c35c43967aad402f66465d8516d3c73b43afcf
Priyanshi77 <sainipriyanshi267@gmail.com>
2026-09-26 15:52:13 +0530

    render api

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(cors({
   origin : [
    "http://localhost:5173",
-  "https://ecommerce-m-all-dusky.vercel.app" 
+  "https://ecommerce-mall-final.onrender.com" 
   ],
   credentials: true
 }));
